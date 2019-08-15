8aa96256ecc17a7619aa48a7b8cacbaf7ffc5af9
kellehet <31115855+kellehet@users.noreply.github.com>
2019-08-15 10:58:22 -0400

    Add files via upload

---
+++ assets/data/flowerData.ts
@@ -0,0 +1,6 @@
+import { Flower } from 'src/app/flower';
+
+export const MYFLOWERS: Flower[] = [
+    { name: 'Rose', color: 'red', country: 'Canada'},
+    { name: 'Tulip', color: 'white', country: 'USA'}
+]
